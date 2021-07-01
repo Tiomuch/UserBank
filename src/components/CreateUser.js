09a6e8e6fdadd8d722e45f4a0e629c81a3910cfa
serzmerz <serzmerz@gmail.com>
2021-07-01 18:15:37 +0300

    add eye for passwords

--- src/components/CreateUser.js
+++ src/components/CreateUser.js
@@ -1,7 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Formik } from 'formik'
 
 function CreateUser() {
+  const [passType, setPassType] = useState('password')
+  const [repPassType, setRepPassType] = useState('password')
+  const [passStyle, setPassStyle] = useState('white')
+  const [repPassStyle, setRepPassStyle] = useState('white')
+
+  const showPass = (e) => {
+    if (e.target.name === 'pass') {
+      if (passType === 'password') {
+        setPassType('text')
+        setPassStyle('black')
+      } else {
+        setPassType('password')
+        setPassStyle('white')
+      }
+    } else if (e.target.name === 'repPass') {
+      if (repPassType === 'password') {
+        setRepPassType('text')
+        setRepPassStyle('black')
+      } else {
+        setRepPassType('password')
+        setRepPassStyle('white')
+      }
+    }
+  }
+
   return (
     <div className="mainFormDiv">
       <h1 className="step">Step 1</h1>
@@ -55,27 +80,32 @@ function CreateUser() {
               className="field"
             />
             {errors.userName && touched.userName && errors.userName}
-            <input
-              type="password"
-              name="password"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.password}
-              placeholder="Password"
-              className="field"
-            />
-            {errors.password && touched.password && errors.password}
-            <input
-              type="repPass"
-              name="repPass"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.repPass}
-              placeholder="Repeat Password"
-              className="field"
-            />
-            {errors.repPass && touched.repPass && errors.repPass}
-            {/*<div className="buttons">*/}
+            <div>
+              <input
+                type={passType}
+                name="password"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password}
+                placeholder="Password"
+                className="field"
+              />
+              <button type="button" className="eye" name="pass" style={{backgroundColor: passStyle}} onClick={showPass}/>
+              {errors.password && touched.password && errors.password}
+            </div>
+            <div>
+              <input
+                type={repPassType}
+                name="repPass"
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.repPass}
+                placeholder="Repeat Password"
+                className="field"
+              />
+              <button type="button" className="eye" name="repPass" style={{backgroundColor: repPassStyle}} onClick={showPass}/>
+              {errors.repPass && touched.repPass && errors.repPass}
+            </div>
             <button type="submit" disabled={isSubmitting} className="firstSubmit">
               Next Step
             </button>
